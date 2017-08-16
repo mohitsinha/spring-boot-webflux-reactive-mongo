21b7350b95d8b86255d79c6e9f7a96ff696c5484
Mohit Sinha <mohitsinha251@gmail.com>
2017-08-16 10:46:05 +0530

    Added functional routes, security configuration

---
+++ src/main/java/com/spring/example/security/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.spring.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.core.userdetails.MapUserDetailsRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsRepository;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    UserDetailsRepository userDetailsRepository() {
+        UserDetails tom = withUsername("tom").password("password").roles("USER").build();
+        UserDetails harry = withUsername("harry").password("password").roles("USER", "ADMIN").build();
+        return new MapUserDetailsRepository(tom, harry);
+    }
+}
